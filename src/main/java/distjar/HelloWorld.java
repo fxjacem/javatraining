18cedab9604d8a26db1f1be74fcf93645b335049
FxJacem <jacem74@gmail.com>
2022-07-31 20:41:56 +0200

    distjar

--- src/main/java/distjar/HelloWorld.java
+++ src/main/java/distjar/HelloWorld.java
@@ -1,10 +1,18 @@
 package distjar;
 
+import java.util.Arrays;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
+
+        System.out.println(args.length);
+
+        if (args.length > 0) {
+            System.out.println(Arrays.asList(args));
+        }
 
     }
 }
