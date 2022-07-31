d5dda6c91ff4b67008cdc8e310f0834b15e6b9e7
FxJacem <jacem74@gmail.com>
2022-07-31 21:20:07 +0200

    distjar

--- src/main/java/distjar/HelloWorld.java
+++ src/main/java/distjar/HelloWorld.java
@@ -1,17 +1,17 @@
 package distjar;
 
-import java.util.Arrays;
-
 public class HelloWorld {
 
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
 
-        System.out.println(args.length);
-
-        if (args.length > 0) {
-            System.out.println(Arrays.asList(args));
+        if (args.length == 0) {
+            System.out.println("No parameter!");
+        } else {
+            for (String arg: args) {
+                System.out.println(arg);
+            }
         }
 
     }
