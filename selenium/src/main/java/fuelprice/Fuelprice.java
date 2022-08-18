248e23222614d7f391e5bff4183ff1d256de9db6
FxJacem <jacem74@gmail.com>
2022-08-18 15:25:06 +0200

    selenium

---
+++ selenium/src/main/java/fuelprice/Fuelprice.java
@@ -0,0 +1,12 @@
+package fuelprice;
+
+public class Fuelprice {
+
+    public static void main(String[] args) {
+
+        String baseUrl = "https://holtankoljak.hu/";
+        String queryUrl = "https://holtankoljak.hu/index.php?ua_map=1&uz_tip=%d&mycity=%d&myrad=%d#page_ad";
+        int[] distance = {1, 5, 10, 20, 30, 40};
+
+    }
+}
