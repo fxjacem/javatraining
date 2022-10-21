12cb915105b29434a51913e8d4b2094829cb281d
FxJacem <jacem74@gmail.com>
2022-10-21 07:16:44 +0200

    refact

--- src/main/java/optionaltask/TechnicalDictionary.java
+++ src/main/java/optionaltask/TechnicalDictionary.java
@@ -4,11 +4,10 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.Collator;
-import java.util.Locale;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.util.*;
 
 public class TechnicalDictionary {
 
@@ -16,11 +15,46 @@ public class TechnicalDictionary {
 
     private static final String RAW_BEGIN = "https://raw.githubusercontent.com";
 
+    private static final String OUTPUT_DIRECTORY = "src/main/resources/";
 
-    public Map<String, String> createDictionary(String url) {
+    public static void main(String[] args){
+
+        TechnicalDictionary rff = new TechnicalDictionary();
+
+        String url = "https://github.com/Strukturavaltas3-Alap-Java/java-strukturavalto3-alap/blob/main/dictionary.md";
+        String filename = "out.md";
+
+        rff.createDictionary(url, filename);
+    }
+
+    public void createDictionary(String url, String filename) {
         String lines = urlReader(createRawUrl(url));
 
-        return processLines(lines);
+        Map<String, String> content = processLines(lines);
+
+        List<String> outputContent = createOutputContent(content);
+
+        writeContentToFile(filename, outputContent);
+    }
+
+    private void writeContentToFile(String filename, List<String> raw) {
+        try {
+            Files.write(Path.of(OUTPUT_DIRECTORY + filename), raw);
+        } catch (IOException ioe) {
+            throw new IllegalStateException("File write error", ioe);
+        }
+    }
+
+    private List<String> createOutputContent(Map<String, String> content) {
+        List<String> outputContent = new ArrayList<>();
+
+        outputContent.add("| Magyar | Angol |");
+        outputContent.add("| --- | --- |");
+        for (Map.Entry<String, String> actual : content.entrySet()){
+            outputContent.add("| " + actual.getKey() + " | " + actual.getValue() + " |");
+        }
+
+        return outputContent;
     }
 
     private Map<String, String> processLines(String content) {
@@ -38,11 +72,13 @@ public class TechnicalDictionary {
     }
     private String urlReader(URL url) {
         String content;
+
         try (Scanner scanner = new Scanner(url.openStream(), String.valueOf(StandardCharsets.UTF_8))) {
             content = scanner.useDelimiter("\\A").next();
         } catch (IOException ioe) {
             throw new IllegalStateException("File read error!", ioe);
         }
+
         return content;
     }
 
@@ -53,18 +89,6 @@ public class TechnicalDictionary {
             return new URL(RAW_BEGIN + parts[1].replace("/blob", ""));
         } catch (MalformedURLException e) {
             throw new IllegalStateException("Wrong url " + url, e);
-        }
-    }
-
-    public static void main(String[] args){
-
-        TechnicalDictionary rff = new TechnicalDictionary();
-        String url = "https://github.com/Strukturavaltas3-Alap-Java/java-strukturavalto3-alap/blob/main/dictionary.md";
-
-        Map<String, String> content = rff.createDictionary(url);
-
-        for (Map.Entry<String, String> actual : content.entrySet()){
-            System.out.println(actual.getKey() + " = " + actual.getValue());
         }
     }
 }
---
+++ src/main/resources/out.md
@@ -0,0 +1,77 @@
+| Magyar | Angol |
+| --- | --- |
+| adatszerkezet | data structure |
+| aktuális paraméter | argument |
+| attribútum | field/attribute |
+| belső osztály | nested class |
+| burkoló osztály | wrapper class |
+| ciklus | loop |
+| címke | label |
+| deklaráció | declaration |
+| egész szám | integer |
+| egyszerű(primitív) típus | primitive type |
+| elágazás | selection |
+| elöl tesztelős ciklus | entry-controlled loop |
+| erősen típusos | strongly typed |
+| értékadó operátor | assignment operator |
+| értékmásolás szerinti paraméterátadás | call by value |
+| explicit típus konverzó | explicit type conversion |
+| folyamatábra | flowchart |
+| fordítani | compile |
+| fordító | compiler |
+| formális paraméter | parameter |
+| futtatás | run |
+| függőség | dependency |
+| gyökérelem | root |
+| hátul tesztelős ciklus | exit-controlled loop |
+| iteráció | iteration |
+| Java Virtuális Gép | Java Virtual Machine |
+| kapcsos zárójel | braces |
+| karakterlánc | char sequence |
+| kifejezés | expression |
+| kiterjeszt | extends |
+| kiterjesztés | extension |
+| kivételkezelés | exception handling |
+| kódolási konvenció | coding convention |
+| kollekció | collection |
+| konkatenáció | concatenation |
+| konstruktor | constructor |
+| láthatósági módosítószó | access modifier |
+| lebegőpontos szám | floating point number |
+| logikai operátor | logical operator |
+| maradékos osztás operator | modulo operator (modulus) |
+| maximumkiválasztás | maximum selection |
+| megszámlálás | counting |
+| megvalósít | implement |
+| megvalósítás (implementáció) | implementation |
+| metódus | method |
+| metódus felüldefiniálása | override |
+| metódusnév túlterhelés | method overloading |
+| módosítható | mutable |
+| objektum állapota | object's state |
+| Objektum Orientált Programozás | Objet Oriented Programming |
+| osztás | division |
+| öröklődés | inheritance |
+| öröklődik | inherits |
+| összegzés | summation |
+| ősosztály | parent class |
+| példány | instance |
+| példányosít | instantiate |
+| példányosítás | instantiation |
+| polimorfizmus | polymorphism |
+| programozási tételek | programming theorems |
+| referencia szerinti paraméterátadás | call by reference |
+| rendező algoritmus | sorting algorithm |
+| sima (kerek) zárójel | parentheses |
+| sortörés karakter | end line character |
+| szorzás | multiplication |
+| szögletes zárójel | brackets |
+| szűrés | filter |
+| tesztlefedettség | test coverage |
+| típuskényszerítés | narrowing casting |
+| utasítás | statement |
+| valós szám | real number |
+| változó | variable |
+| verem | stack |
+| visszatérési érték | return value |
+| visszatérési típus | return type/result type |
